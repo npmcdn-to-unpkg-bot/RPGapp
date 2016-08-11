2d007310cd502033c6606470c9c7c9949679cd78
Michael Guo <mdguo@uwaterloo.ca>
2016-08-10 22:06:01 -0400

    added upgrades

--- js/item.js
+++ js/item.js
@@ -13,14 +13,40 @@ RPG.Item.initItem = function(_i_type) {
 
 	var item = {}
 
+	item.itemType = _i_type
+
 	if(_i_type == RPG.Item.Type.Weapon) {
 		item.damage = 10
+		item.canUpgrade = true
+		item.upgradeStatus = 0
 	} else if(_i_type == RPG.Item.Type.Armor) {
 		item.defense = 6
 		item.protection = 4
+		item.canUpgrade = true
+		item.upgradeStatus = 0
 	} else {
 		item.defense = 2
 	}
 
 	return item
+}
+
+RPG.Item.upgradeWeapon = function(weapon) {
+	if(weapon.itemType != RPG.Item.Type.Weapon) return false
+	if(weapon.upgradeStatus > 4) return false
+
+	weapon.damage += 2
+	weapon.upgradeStatus++
+
+	return weapon
+}
+
+RPG.Item.upgradeArmor = function(armor) {
+	if(weapon.itemType != RPG.Item.Type.Armor) return false
+	if(weapon.upgradeStatus > 4) return false
+
+	armor.defense += 2
+	armor.protection += 1
+
+	return armor
 }
